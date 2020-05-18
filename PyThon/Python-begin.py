56962e29008236ab9ade20476a605f9a1730224b
minh1991 <hikari.mei.119@gmail.com>
2020-05-18 22:19:08 +0700

    PyThon Dict 20200518

---
+++ PyThon/Python-begin.py
@@ -0,0 +1,10 @@
+# Khởi tạo Dict
+name    =   ''
+Age     =   0
+comment =   ''
+T_Dict = dict(name='Micheal',Age=15,comment='OK')
+
+
+# Call Dict
+print('All Dict:    ',T_Dict)
+print('--------------------')
